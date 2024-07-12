528f11abc7fdd628b0bd5b3b4ab028fa47d44849
Parijat Ghosh <paarijatghosh@gmail.com>
2024-07-12 23:59:34 +0530

    Initial commit

--- consts/consts.go
+++ consts/consts.go
@@ -13,11 +13,11 @@ import (
 
 const (
 	// TODO: choose a human-readable part for your hyperchain
-	HRP = ""
+	HRP = "new"
 	// TODO: choose a name for your hyperchain
-	Name = ""
+	Name = "ParijatCoin"
 	// TODO: choose a token symbol
-	Symbol = ""
+	Symbol = "PJT"
 )
 
 var ID ids.ID
